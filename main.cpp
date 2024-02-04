9d5664bf93abfbe3762c2beb55633d1343dd5e76
ntotev <158792743+ntotev@users.noreply.github.com>
2024-02-04 04:01:33 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "Hello World!\n";
+cout << "dont put any spaces in any of the following questions.\n";
+  string name;
+cout << "what is your name? ";
+  cin >> name;
+string num;
+  cout << "what is your phones passcode? ";
+  cin >> num;
+    string adr;
+  cout << "WHERE DO YOU LIVE?! ";
+  cin >> adr;
+    cout << "Hi i am " + name + " my phones passcode is " + num + " and i live at " + adr + "\n";
+  
+  
+  
+  
+  
+  }
